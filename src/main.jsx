eed23fa060b518f461d3b785b5b83df0051d13ad
Sudarshan Mishra <sudarshan03in@gmail.com>
2026-06-29 13:50:36 +0530

    Add Supabase auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import AuthWrapper from './AuthWrapper'
 import UPSCTracker from './upsc_tracker.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <AuthWrapper>
     <UPSCTracker />
-  </StrictMode>,
+  </AuthWrapper>,
 )
